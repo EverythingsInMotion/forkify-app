1488b750fbe9ec1502cafffe4e64ac33ba6bf61f
EverythingsInMotion <sourabhck.ca@gmail.com>
2023-05-16 22:04:14 +0530

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -142,6 +142,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 }
 
+const newFeature = function () {
+  console.log("Welcome to the application");
+}
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks)
   recipeView.addHandlerRender(controlRecipes);
@@ -150,6 +154,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 
 init();
